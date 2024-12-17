4c4ca79fbfbce78a4a2273035dda63e5d100f727
jaspershen <shenxt1990@outlook.com>
2024-12-17 13:58:42 +0800

    update

--- 1_code/100_tools.R
+++ 1_code/100_tools.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 library(ggplot2)
 
+<<<<<<< Updated upstream
 
 
+=======
+group_color <-
+  c(
+    "BA52" = "#BC3C29FF",
+    "BQ11" = "#E18727FF",
+    "WT" = "#EE4C97FF",
+    "Ctrl" = "#20854EFF",
+    "QC" = "#FFDC91FF"
+  )
+>>>>>>> Stashed changes
